95997b1177d32a42e625a5ab7c597d985c4266a0
Neil Vodoor <19353141+nvodoor@users.noreply.github.com>
2017-11-21 18:15:20 -0800

    React Boilerplate

---
+++ ReactBoilerPlate2/app/Components/Main.jsx
@@ -0,0 +1,16 @@
+var React = require('react');
+
+
+var Main = (props) => {
+		return (
+		<div>
+			<div>
+				{props.children}
+			</div>	
+		</div>
+	  );
+}
+
+
+module.exports = Main;
+
---
+++ ReactBoilerPlate2/webpack.config.js
@@ -0,0 +1,41 @@
+var webpack = require('webpack')
+
+module.exports = {
+	entry: ['script!jquery/dist/jquery.min.js','script!foundation-sites/dist/foundation.min.js','./app/app.jsx',], 
+	output: {
+		//dirname gives you path to file you are in - In this case ReactBundle
+		path: __dirname,
+		filename: './public/bundle.js'
+	},
+	externals: {
+		jquery: 'jQuery'
+	},
+	plugins: [
+		new webpack.ProvidePlugin({
+			'$':'jquery',
+			'jQuery':'jquery'
+		})
+	],
+	resolve: {
+		root: __dirname,
+		// alias gives name to components and tells webpack where to find em
+		alias:  {
+			Main: 'app/Components/Main.jsx',
+			applicationStyles: 'app/styles/app.scss'
+		},
+		extensions: ['','.js','.jsx']
+	},
+	module: {
+		loaders: [
+			{
+				loader: 'babel-loader',
+				query: {
+					presets: ['react', 'es2015']
+				},
+				// test refers to all the files of a type of extension you want to grab
+				test: /\.jsx?$/,
+				exclude: /(node_modules|bower_components)/
+			}
+		]
+	}
+};
